d28b3a8de3a3e093441748f88b39a48b20bcb980
JulioneQA <neroazurro@gmail.com>
2025-05-04 09:44:52 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-5f5189d0-c196-49dc-a603-fc99407a8d9a.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-ea7b026e-a4cd-4a2f-9f0a-abb8abe97fc9.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
